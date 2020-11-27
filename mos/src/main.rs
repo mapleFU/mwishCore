4de6b1ae425785a78fdf1f2060ff3f62bb513f3b
mwish <anmmscs_maple@qq.com>
2020-11-27 15:13:15 +0800

    [add] adding basic logic (#1)

--- mos/src/main.rs
+++ mos/src/main.rs
@@ -14,51 +14,24 @@
 //! - `#![feature(global_asm)]`
 //!   内嵌整个汇编文件
 #![feature(global_asm)]
+//!
+//! - `#![feature(panic_info_message)]`  
+//!   panic! 时，获取其中的信息并打印
+#![feature(panic_info_message)]
 
+#[macro_use]
+mod console;
+mod panic;
+mod sbi;
+
+// 汇编编写的程序入口，具体见该文件
 global_asm!(include_str!("entry.asm"));
-
-use core::panic::PanicInfo;
-
-/// 当 panic 发生时会调用该函数
-/// 我们暂时将它的实现为一个死循环
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-// /// 覆盖 crt0 中的 _start 函数
-// /// 我们暂时将它的实现为一个死循环
-// #[no_mangle]
-// pub extern "C" fn _start() -> ! {
-//     loop {}
-// }
-
-/// 在屏幕上输出一个字符，目前我们先不用了解其实现原理
-pub fn console_putchar(ch: u8) {
-    let _ret: usize;
-    let arg0: usize = ch as usize;
-    let arg1: usize = 0;
-    let arg2: usize = 0;
-    let which: usize = 1;
-    unsafe {
-        llvm_asm!("ecall"
-             : "={x10}" (_ret)
-             : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
-             : "memory"
-             : "volatile"
-        );
-    }
-}
 
 /// Rust 的入口函数
 ///
 /// 在 `_start` 为我们进行了一系列准备之后，这是第一个被调用的 Rust 函数
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
-    // 在屏幕上输出 "OK\n" ，随后进入死循环
-    console_putchar(b'O');
-    console_putchar(b'K');
-    console_putchar(b'\n');
-
-    loop {}
+    println!("Hello mwishCore!");
+    panic!("end of rust_main")
 }
